66cdfa4519968d1fd43d8fc294932b48fe1a4d25
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-08-01 14:26:52 +0530

    314. Edit Task - Complete

--- src/SingleItem.jsx
+++ src/SingleItem.jsx
@@ -1,10 +1,24 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import customFetch from './utils';
+
 const SingleItem = ({ item }) => {
+  const queryClient = useQueryClient();
+
+  const { mutate: editTask } = useMutation({
+    mutationFn: ({ taskId, isDone }) => {
+      return customFetch.patch(`/${taskId}`, { isDone });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['tasks'] });
+    },
+  });
+
   return (
-    <div className='single-item'>
+    <div className="single-item">
       <input
-        type='checkbox'
+        type="checkbox"
         checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        onChange={() => editTask({ taskId: item.id, isDone: !item.isDone })}
       />
       <p
         style={{
@@ -15,8 +29,8 @@ const SingleItem = ({ item }) => {
         {item.title}
       </p>
       <button
-        className='btn remove-btn'
-        type='button'
+        className="btn remove-btn"
+        type="button"
         onClick={() => console.log('delete task')}
       >
         delete
